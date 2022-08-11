ee68bbe4b1920979c900ba15a0a49e446f8cc1b4
kazustudy0107 <the.stone.monky.6317@gmail.com>
2022-08-11 15:33:01 +0900

    ♻️ index.tsxをリファクタリング

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,4 @@
-import { Button, Container, Group } from "@mantine/core";
 import type { NextPage } from "next";
-import Link from "next/link";
-import { Layout } from "../components/Layout";
-import { Moon } from "tabler-icons-react";
 import { Header } from "./header";
 
 const Home: NextPage = () => {
