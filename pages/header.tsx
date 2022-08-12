376e8b13ac7e4a608dd3e1978c9c1b6cd3307706
kazustudy0107 <the.stone.monky.6317@gmail.com>
2022-08-13 06:41:16 +0900

    🐛 headerを修正

--- pages/header.tsx
+++ pages/header.tsx
@@ -1,4 +1,4 @@
-import { Button, Container, Group } from "@mantine/core";
+import { ActionIcon, Button, Container, Group } from "@mantine/core";
 import Link from "next/link";
 import { Layout } from "../components/Layout";
 import { Moon } from "tabler-icons-react";
@@ -6,30 +6,27 @@ import { Moon } from "tabler-icons-react";
 export const Header = () => {
   return (
     <Layout title="header">
-        <Container className="h-16 flex place-content-between font-bold">
-          <Group className="text-xl">
-            <Link href="/">Shimabu IT University</Link>
-          </Group>
+      <Container className="flex h-16 place-content-between font-bold">
+        <Group className="text-xl">
+          <Link href="/">Shimabu IT University</Link>
+        </Group>
 
-          <Group className="flex text-lg">
-            <Link href="/about">
-              <a className="m-2 gap-3 py-2">About</a>
-            </Link>
-            <Link href="/blog">
-              <a className="m-2 gap-3 py-2">Blog</a>
-            </Link>
-            <Link href="/Portfolio">
-              <a className="m-2 gap-3 py-2">Portfolio</a>
-            </Link>
-            <Link href="/Contact">
-              <a className="m-2 gap-3 py-2">Contact</a>
-            </Link>
-            <Button variant="outline" color="dark" size="xs">
-              {<Moon />}
-              
-            </Button>
-          </Group>
-        </Container>
-      </Layout>
+        <Group className="flex text-lg">
+          <Link href="/about">
+            <a className="m-2 gap-3 py-2">About</a>
+          </Link>
+          <Link href="/blog">
+            <a className="m-2 gap-3 py-2">Blog</a>
+          </Link>
+          <Link href="/Portfolio">
+            <a className="m-2 gap-3 py-2">Portfolio</a>
+          </Link>
+          <Link href="/Contact">
+            <a className="m-2 gap-3 py-2">Contact</a>
+          </Link>
+          <ActionIcon size="lg" variant="outline">{<Moon />}</ActionIcon>
+        </Group>
+      </Container>
+    </Layout>
   );
 };
