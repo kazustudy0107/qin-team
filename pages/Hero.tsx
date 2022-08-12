3758742783178eb140704333687a145ee217cc84
kazustudy0107 <the.stone.monky.6317@gmail.com>
2022-08-12 18:03:56 +0900

    🎨 Heroページを作成

---
+++ pages/Hero.tsx
@@ -0,0 +1,24 @@
+import { Container, Group, Title } from "@mantine/core";
+import Link from "next/link";
+import { BrandTwitter, BrandFacebook, Rss } from "tabler-icons-react";
+import { Layout } from "../components/Layout";
+
+export const Hero = () => {
+  return (
+    <Layout title="Hero">
+      <Container className="text-center bg-pink-600 text-white flex justify-between">
+        <Group className="flex-col ">
+          <Title className="text-4xl mt-12">Shimabu IT University</Title>
+          <text className="text-base mb-14">
+            しまぶーのポートフォリオのためのページです
+          </text>
+        </Group>
+        <Group>
+          <Link href="/">{<BrandTwitter />}</Link>
+          <Link href="/">{<BrandFacebook />}</Link>
+          <Link href="/">{<Rss />}</Link>
+        </Group>
+      </Container>
+    </Layout>
+  );
+};
