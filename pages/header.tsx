16f4cdde675de7abdce1711aa8e78d22788c505b
kazustudy0107 <the.stone.monky.6317@gmail.com>
2022-08-13 16:31:03 +0900

    🐛 header部分を修正

--- pages/header.tsx
+++ pages/header.tsx
@@ -1,11 +1,11 @@
-import { ActionIcon, Button, Container, Group } from "@mantine/core";
+import { ActionIcon, Container, Group } from "@mantine/core";
 import Link from "next/link";
 import { Layout } from "../components/Layout";
 import { Moon } from "tabler-icons-react";
 
 export const Header = () => {
   return (
-    <Layout title="header">
+    <Layout title="header" >
       <Container className="flex h-16 place-content-between font-bold">
         <Group className="text-xl">
           <Link href="/">Shimabu IT University</Link>
