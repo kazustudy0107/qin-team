b067a1c7801c1ff3096f78661ea23fe0abe9530f
kazustudy0107 <the.stone.monky.6317@gmail.com>
2022-08-13 17:52:25 +0900

    🎨 Portfolio部分を作成

---
+++ pages/Portfolio.tsx
@@ -0,0 +1,20 @@
+import { Button, Center, Container } from "@mantine/core";
+import React from "react";
+import { Layout } from "../components/Layout";
+import { CustomCard } from "./CustomCard";
+
+export const Portfolio = () => {
+  return (
+    <Layout title="Portfolio">
+      <Container className="mt-24">
+        <h1 className="mb-5 text-2xl font-bold">Portfolio</h1>
+        <CustomCard />
+      </Container>
+      <Center>
+        <Button color="dark" radius="xl" size="xs">
+          View All
+        </Button>
+      </Center>
+    </Layout>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import { Blog } from "./blog";
 import { Header } from "./header";
 import { Hero } from "./Hero";
+import { Portfolio } from "./Portfolio";
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +10,7 @@ const Home: NextPage = () => {
       <Header />
       <Hero />
       <Blog />
+      <Portfolio />
     </div>
   );
 };
