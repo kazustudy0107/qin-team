a198a5d9724e0c420b145d43abf71503c28a3fa7
kazustudy0107 <the.stone.monky.6317@gmail.com>
2022-10-02 16:17:09 +0900

    🐛 client.tsを変更

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -2,6 +2,6 @@ import { createClient } from "microcms-js-sdk"; //ES6
 
 // Initialize Client SDK.
 export const client = createClient({
-  serviceDomain: "team-develop-blog", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: process.env.API_KEY, // API KEY is the API key you created in the API settings
+  serviceDomain: process.env.SERVICE_DOMAIN || "", 
+  apiKey: process.env.API_KEY || "", 
 });
